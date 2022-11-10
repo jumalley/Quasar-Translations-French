0221cf16f5f3a08d173dd3ccdb522a4645fee389
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:50:10 +0100

    Add files via upload

---
+++ qs-shops/translations.lua
@@ -0,0 +1,87 @@
+--████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+--╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+--░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+--░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+--░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+--░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+Config.Language = "fr" --You can choose between 'en', 'es' or create your own language.
+
+Config.Languages = { --You can copy one of these translations and create your own with another language.
+    ['en'] = {
+        ["NEED_LICENSE"] = 'You dont have the license',
+        ["MENU_TITLE_CUSTOMSHOP"] = "Custom Shop Menu",
+        ["CREATE_SHOP"] = "Create Shop",
+        ["DELETE_SHOP"] = "Delete Shop",
+        ["ADD_ITEM"] = "Add Item",
+        ["INSERT_NAME_SHOP"] = 'Insert the name of the shop',
+
+        ["MENU_TITLE_DELETE_SHOP"] = "Are you sure about delete this shop?",
+        ["DELETE_SHOP_YES"] = "Yes",
+        ["DELETE_SHOP_NO"] = 'No',
+
+        ["INSERT_NAME_ITEM"] = 'Insert the name of the item',
+        ["INSERT_AMOUNT_ITEM"] = 'Insert the amount of the item',
+        ["INSERT_PRICE_ITEM"] = 'Insert the price of the item',
+
+        ["INVENTORY"] = 'Inventory',
+
+        ["INVENTORY_YOU_BUY"] = 'You bought ',
+        ["NO_MONEY"] = 'Not enough money',
+        ["NO_STOCK"] = 'Not enough stock',
+
+        ["PLAYERSHOP_NAME_NEEDED"] = "You need to insert the name of the shop.",
+        ["PLAYERSHOP_ANOTHER_SHOP"] = "You are too close to another shop.",
+        ["PLAYERSHOP_NAME_ALREADY"] = "A shop with this name already exists!",
+        ["PLAYERSHOP_REQUIRED_JOB"] = "You do not have the Required Job:",
+        ["PLAYERSHOP_REQUIRED_ITEM"] = "You do not have the required items",
+        ["PLAYERSHOP_NO_WEAPONS"] = "Weapons cannot be sold in private shops...",
+        ["PLAYERSHOP_NO_SHOPS"] = "You are not close to any shop.",
+        ["PLAYERSHOP_NOT_OWNER"] = "You do not own this shop!",
+        ["PLAYERSHOP_DELETE"] = "You deleted the store: ",
+        ["PLAYERSHOP_SEIZED"] = "This Shop was siezed successfully!",
+        ["PLAYERSHOP_ADDED"] = "Added:",
+        ["PLAYERSHOP_TO"] = "to ",
+        ["PLAYERSHOP_FOR"] = "for $",
+        ["PLAYERSHOP_EACH"] = "each.",
+        ["PLAYERSHOP_TEXT"] = "[E] -",
+    },
+    ['fr'] = {
+        ["NEED_LICENSE"] = 'Vous ne possèdez pas le permis nécessaire.',
+        ["MENU_TITLE_CUSTOMSHOP"] = "Magasin",
+        ["CREATE_SHOP"] = "Créer un magasin",
+        ["DELETE_SHOP"] = "Supprimer un magasin",
+        ["ADD_ITEM"] = "Ajouter un objet",
+        ["INSERT_NAME_SHOP"] = 'Choisissez un nom pour le magasin',
+
+        ["MENU_TITLE_DELETE_SHOP"] = "Êtes vous sûr?",
+        ["DELETE_SHOP_YES"] = "Oui",
+        ["DELETE_SHOP_NO"] = 'Non',
+
+        ["INSERT_NAME_ITEM"] = 'Choisissez un nom pour votre objet',
+        ["INSERT_AMOUNT_ITEM"] = 'Choisissez une quantité',
+        ["INSERT_PRICE_ITEM"] = 'Choisissez un prix pour votre objet',
+
+        ["INVENTORY"] = 'Inventaire',
+
+        ["INVENTORY_YOU_BUY"] = 'Vous avez acheté ',
+        ["NO_MONEY"] = 'Vous n\'avez pas assez d\'argent.',
+        ["NO_STOCK"] = 'Il n\'y a plus de stock.',
+
+        ["PLAYERSHOP_NAME_NEEDED"] = "Vous devez insérer le nom de la boutique.",
+        ["PLAYERSHOP_ANOTHER_SHOP"] = "Vous êtes trop proche d'un autre magasin.",
+        ["PLAYERSHOP_NAME_ALREADY"] = "Un magasin portant ce nom existe déjà!",
+        ["PLAYERSHOP_REQUIRED_JOB"] = "Vous n'avez pas le travail requis:",
+        ["PLAYERSHOP_REQUIRED_ITEM"] = "Vous n'avez pas les éléments requis.",
+        ["PLAYERSHOP_NO_WEAPONS"] = "Les armes ne peuvent pas être vendues dans les magasins privés...",
+        ["PLAYERSHOP_NO_SHOPS"] = "Vous devez être à proximité d'un magasin.",
+        ["PLAYERSHOP_NOT_OWNER"] = "Vous n'êtes pas propriétaire de ce magasin !",
+        ["PLAYERSHOP_DELETE"] = "Vous avez supprimé le magasin: ",
+        ["PLAYERSHOP_SEIZED"] = "Cette boutique a été saisie avec succès!",
+        ["PLAYERSHOP_ADDED"] = "Ajouté:",
+        ["PLAYERSHOP_TO"] = "vers ",
+        ["PLAYERSHOP_FOR"] = "pour €",
+        ["PLAYERSHOP_EACH"] = "chaque.",
+        ["PLAYERSHOP_TEXT"] = "[E] -",
+    },
+}
