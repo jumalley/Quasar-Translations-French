4bddb86e5d0a0120bcc58d15fb0d56b73cf31039
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:57:07 +0100

    Add files via upload

---
+++ [casino]/qs-casino/translations.lua
@@ -0,0 +1,27 @@
+Config.Language = 'en'
+
+Config.Languages = {
+    ['en'] = {
+        ["CASINO_ENTER"] = "Appuyez sur [E] pour rentrer.",
+        ["CASINO_EXIT"] = "Appuyez sur [E] pour sortir.",
+        ["CASINO_CASHIER"] = "Appuyez sur [E] pour acheter des jetons.",
+
+        ["NO_MONEY"] = "Vous n'avez pas assez d'argent.",
+        ["NO_CHIPS"] = "Vous n'avez pas de jetons à échanger.",
+        ["BUY_TICKETS"] = "Vous avez acheté un ticket pour la roue de la fortune.",
+        ["BUY_CHIPS"] = "Vous avez acheté des jetons.",
+        ["SELL_CHIPS"] = "Vous avez échangé des jetons.",
+    },
+
+    ['es'] = {
+        ["CASINO_ENTER"] = "[E] - Entrar",
+        ["CASINO_EXIT"] = "[E] - Salir",
+        ["CASINO_CASHIER"] = "[E] - Cajero",
+
+        ["NO_MONEY"] = "No tienes suficiente dinero",
+        ["NO_CHIPS"] = "No tienes fichas para vender",
+        ["BUY_TICKETS"] = "Compraste boletos para Lucky Wheel",
+        ["BUY_CHIPS"] = "Compraste fichas",
+        ["SELL_CHIPS"] = "Vendiste fichas",
+	}
+}
