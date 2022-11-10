49a75fb26b4fbd1b50199117ac63d59b6b8934dd
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:19:02 +0100

    Add files via upload

---
+++ qs-armor/translations.lua
@@ -0,0 +1,18 @@
+Config.Language = "fr" --You can choose between 'en', 'es', 'it' or create your own language.
+
+Config.Languages = { --You can copy one of these translations and create your own with another language.
+    ['en'] = {
+        ["ALREADY_HAVE_VEST"] = "You already have a bulletproof vest",
+        ["DONT_HAVE_VEST"] = "You don't have a bulletproof vest",
+        ["USE_ARMOR"] = "Placing Armor ...",
+        ["USE_HEAVYARMOR"] = "Placing Heavy Armor ...",
+        ["REMOVE_ARMOR"] = "Removing Armor ...",
+    },
+    ['fr'] = {
+        ["ALREADY_HAVE_VEST"] = "Vous avez déjà un kevlar.",
+        ["DONT_HAVE_VEST"] = "Vous n'avez pas de kevlar.",
+        ["USE_ARMOR"] = "Equipement du kevlar...",
+        ["USE_HEAVYARMOR"] = "Equipement du kevlar...",
+        ["REMOVE_ARMOR"] = "Suppression du kevlar...",
+    },
+}
