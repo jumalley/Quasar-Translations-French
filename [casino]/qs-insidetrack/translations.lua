b2455cf1cd200ffe4084f62de6286c6f1e682f96
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:59:01 +0100

    Add files via upload

---
+++ [casino]/qs-insidetrack/translations.lua
@@ -0,0 +1,11 @@
+Config.Language = 'en'
+
+Config.Languages = {
+    ['en'] = {
+        ["PLAY_INSIDETRACK"] = "[E] - Insidetrack",
+    },
+
+    ['es'] = {
+        ["PLAY_INSIDETRACK"] = "[E] - Insidetrack",
+	}
+}
