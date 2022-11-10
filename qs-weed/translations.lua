c9d019f7c70f5a47513a5eeb8c8581c4187c65ec
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:56:02 +0100

    Add files via upload

---
+++ qs-weed/translations.lua
@@ -0,0 +1,71 @@
+--████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+--╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+--░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+--░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+--░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+--░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+
+Config.Language = 'fr' -- Available languages: 'en' , 'es' or create yours in the latest settings.
+
+Config.Languages = { -- You can modify your language or even create new languages ​​here.
+    ['en'] = {
+        ["WEED_DRAWTEXT_MOVE_PLANT"] = "[G] - Move Plant",
+        ["WEED_DRAWTEXT_BURN_PLANT"] = "[E] - Burn Plant",
+        ["WEED_DRAWTEXT_GENDER_MALE"] = "M",
+        ["WEED_DRAWTEXT_GENDER_FEMALE"] = "F",
+        ["WEED_DRAWTEXT_INFORMATION_TYPE"] = "Type:",
+        ["WEED_DRAWTEXT_INFORMATION_NUTRITION"] = "Nutrition:",
+        ["WEED_DRAWTEXT_INFORMATION_HEALTH"] = "Health:",
+        ["WEED_DRAWTEXT_INFORMATION_HARVESTABLE"] = "Plant is fully grown, press [E] to harvest",
+        ["WEED_DRAWTEXT_INFORMATION_DEAD"] = "The plant is dead, press [E] to remove the plant",
+
+        ["WEED_NOTIFICATION_LEAVE_PLANT"] = "You left the plant in the ground",
+        ["WEED_NOTIFICATION_MOVING_PLANT"] = "You moved the plant",
+        ["WEED_NOTIFICATION_THERES_NO_SPACE"] = "There is no space for the plant",
+        ["WEED_NOTIFICATION_WRONG_ZONE"] = "There is no house nearby",
+        ["WEED_NOTIFICATION_NOT_A_PLANT"] = "There is no plant nearby",
+        ["WEED_NOTIFICATION_INVALID_PLANT"] = "Plant no longer exists",
+        ["WEED_NOTIFICATION_INVALID_HOUSE"] = "You cannot plant in this house",
+        ["WEED_NOTIFICATION_HEALTHY_PLANT"] = "The plant does not need more nutrition",
+        ["WEED_NOTIFICATION_NO_BAGS"] = "You don't have enough bags",
+        ["WEED_NOTIFICATION_REMOVING_PLANT"] = "A plant was harvested",
+        ["WEED_NOTIFICATION_REMOVING_POLICE"] = "You set the plant on fire",
+        ["WEED_NOTIFICATION_REMOVING_POLICE_SUCCESS"] = "The plant was completely burned",
+
+        ["WEED_PROGRESS_PLANTING_PLANT"] = "Planting marijuana...",
+        ["WEED_PROGRESS_GROWING_PLANT"] = "Growing plant...",
+        ["WEED_PROGRESS_NUTRITING_PLANT"] = "Nuturing plant...",
+        ["WEED_PROGRESS_REMOVING_PLANT"] = "Removing plant...",
+	},
+
+  ['fr'] = {
+  ["WEED_DRAWTEXT_MOVE_PLANT"] = "[G] - Déplacer la plante",
+  ["WEED_DRAWTEXT_BURN_PLANT"] = "[E] - Brûler la plante",
+  ["WEED_DRAWTEXT_GENDER_MALE"] = "M",
+  ["WEED_DRAWTEXT_GENDER_FEMALE"] = "F",
+  ["WEED_DRAWTEXT_INFORMATION_TYPE"] = "Tapez :",
+  ["WEED_DRAWTEXT_INFORMATION_NUTRITION"] = "Alimentation :",
+  ["WEED_DRAWTEXT_INFORMATION_HEALTH"] = "Santé :",
+  ["WEED_DRAWTEXT_INFORMATION_HARVESTABLE"] = "La plante est complètement développée, appuyez sur [E] pour récolter",
+  ["WEED_DRAWTEXT_INFORMATION_DEAD"] = "La plante est morte, appuyez sur [E] pour supprimer la plante",
+
+  ["WEED_NOTIFICATION_LEAVE_PLANT"] = "Vous avez laissé la plante en terre",
+  ["WEED_NOTIFICATION_MOVING_PLANT"] = "Vous avez déplacé la plante",
+  ["WEED_NOTIFICATION_THERES_NO_SPACE"] = "Il n'y a pas d'espace pour la plante",
+  ["WEED_NOTIFICATION_WRONG_ZONE"] = "Il n'y a pas de maison à proximité",
+  ["WEED_NOTIFICATION_NOT_A_PLANT"] = "Il n'y a pas de plante à proximité",
+  ["WEED_NOTIFICATION_INVALID_PLANT"] = "La plante n'existe plus",
+  ["WEED_NOTIFICATION_INVALID_HOUSE"] = "Vous ne pouvez pas planter dans cette maison",
+  ["WEED_NOTIFICATION_HEALTHY_PLANT"] = "La plante n'a pas besoin de plus de nutrition",
+  ["WEED_NOTIFICATION_NO_BAGS"] = "Vous n'avez pas assez de sacs",
+  ["WEED_NOTIFICATION_REMOVING_PLANT"] = "Une plante a été récoltée",
+  ["WEED_NOTIFICATION_REMOVING_POLICE"] = "Vous avez mis le feu à la plante",
+  ["WEED_NOTIFICATION_REMOVING_POLICE_SUCCESS"] = "La plante a été complètement brûlée",
+
+  ["WEED_PROGRESS_PLANTING_PLANT"] = "Planter du cannabis...",
+  ["WEED_PROGRESS_GROWING_PLANT"] = "Culture de la plante...",
+  ["WEED_PROGRESS_NUTRITING_PLANT"] = "Nutrition de la plante...",
+  ["WEED_PROGRESS_REMOVING_PLANT"] = "Enlever la plante...",
+  },
+}
