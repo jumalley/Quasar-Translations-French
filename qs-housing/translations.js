1691c40d95ffc3ff1549d491e86bb3ceb361d993
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:35:01 +0100

    Add files via upload

---
+++ qs-housing/translations.js
@@ -0,0 +1,62 @@
+
+//████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+//╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+//░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+//░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+//░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+//░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+// Full translation of html placeholders.
+$(".decorate-items-filter").attr('placeholder', 'Filtrer les objets...');
+
+// Full translation of html texts.
+var app = new Vue({ el: '.general-container', data: {
+
+  HOUSING_NUI_PROPERTY_ADDRESS : "Adresse de la propriété",
+    HOUSING_NUI_PROPERTY_PRICE : "Prix de l'immobilier",
+    HOUSING_NUI_PROPERTY_AGFEES : "Frais d'agence (5%)",
+    HOUSING_NUI_PROPERTY_BAFEES : "Frais bancaires (10 %)",
+    HOUSING_NUI_PROPERTY_TAXES : "Taxes (6 %)",
+    HOUSING_NUI_PROPERTY_CRFEES : "Frais de crédit horaire",
+    HOUSING_NUI_PROPERTY_FINAL_PRICE : "Prix final",
+    HOUSING_NUI_PROPERTY_CANCEL : "Annuler",
+    HOUSING_NUI_PROPERTY_BUY : "Acheter la maison",
+    HOUSING_NUI_PROPERTY_CREDIT : "Acheter avec crédit",
+
+    HOUSING_NUI_DECORATE_FREEMODE : "MODE LIBRE",
+    HOUSING_NUI_DECORATE_BUY_OBJECT : "Êtes-vous sûr de vouloir acheter l'objet ?",
+    HOUSING_NUI_DECORATE_MY_OBJECT : "Options de mes objets",
+    HOUSING_NUI_DECORATE_SAVE_OBJECT : "Confirmez-vous la position des objets ?",
+    HOUSING_NUI_DECORATE_OPTION_STASH : "Stocker",
+    HOUSING_NUI_DECORATE_OPTION_SELL : "Vendre",
+    HOUSING_NUI_DECORATE_OPTION_MOVE : "Déménager",
+    HOUSING_NUI_DECORATE_ACCEPT : "Oui",
+    HOUSING_NUI_DECORATE_CANCEL : "Non",
+    HOUSING_NUI_DECORATE_OPTIONS_NEXT : "Suivant",
+    HOUSING_NUI_DECORATE_OPTIONS_PREVIOUS : "Précédent",
+    HOUSING_NUI_DECORATE_OPTIONS_0 : "Salon",
+    HOUSING_NUI_DECORATE_OPTIONS_1 : "Cuisine",
+    HOUSING_NUI_DECORATE_OPTIONS_2 : "Salle de bain",
+    HOUSING_NUI_DECORATE_OPTIONS_3 : "Chambre",
+
+    HOUSING_NUI_INFORMATION_TITLE : "INFORMATIONS",
+    HOUSING_NUI_INFORMATION_MOVE_CAMERA : "Souris",
+    HOUSING_NUI_INFORMATION_MOVE_CAMERA_DESCRIPTION : "pour déplacer la caméra",
+    HOUSING_NUI_INFORMATION_MOVE_OBJECT : "Clic gauche + Glisser",
+    HOUSING_NUI_INFORMATION_MOVE_OBJECT_DESCRIPTION : "pour déplacer les objets",
+    HOUSING_NUI_INFORMATION_ROTATION : "Molette de la souris",
+    HOUSING_NUI_INFORMATION_ROTATION_DESCRIPTION : "modifie la hauteur de l'objet",
+    HOUSING_NUI_INFORMATION_SAVE : "Entrez",
+    HOUSING_NUI_INFORMATION_SAVE_DESCRIPTION : "pour acheter et placer l'objet",
+    HOUSING_NUI_INFORMATION_FOCUS : "F5",
+    HOUSING_NUI_INFORMATION_FOCUS_DESCRIPTION : "pour afficher/masquer la souris",
+    HOUSING_NUI_INFORMATION_HEIGHT : "Flèche",
+    HOUSING_NUI_INFORMATION_HEIGHT_DESCRIPTION : "modifier la hauteur et l'axe",
+    HOUSING_NUI_INFORMATION_UP : "E",
+    HOUSING_NUI_INFORMATION_UP_DESCRIPTION : "télécharger la caméra dans la décoration",
+    HOUSING_NUI_INFORMATION_LOW : "Q",
+    HOUSING_NUI_INFORMATION_LOW_DESCRIPTION : "caméra inférieure en décoration",
+    HOUSING_NUI_INFORMATION_FLOOR : "Alt gauche",
+    HOUSING_NUI_INFORMATION_FLOOR_DESCRIPTION : "placer les meubles contre le sol",
+    },
+})
