77e373994f88b372bd1bf40aa12436f538256a2c
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:29:55 +0100

    Add files via upload

---
+++ qs-evidence/translations.lua
@@ -0,0 +1,28 @@
+--████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+--╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+--░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+--░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+--░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+--░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+Config.Language = 'fr'
+
+Config.Languages = {
+    ['fr'] = {
+        ["CLEAN_AREA"] = "Douilles prises",
+        ["TAKE_EVIDENCE"] = "Vous avez recupéré la preuve",
+        ["YOU_NEED_BAG"] = "Vous devez avoir un sac de preuves vide avec vous.",
+        ["BULLET_LABEL"] = "Douilles",
+
+        ["BULLET_EVIDENCE"] = "[G] - Preuves - ~r~#",
+    },
+
+    ['es'] = {
+        ["CLEAN_AREA"] = "Casquillos de bala capturados",
+        ["TAKE_EVIDENCE"] = "Guardaste la evidencia",
+        ["YOU_NEED_BAG"] = "Necesitas tener una bolsa de pruebas vacía contigo",
+        ["BULLET_LABEL"] = "Evolución de bala",
+
+        ["BULLET_EVIDENCE"] = "[G] - Prueba de bala - ~r~#",
+	}
+}
