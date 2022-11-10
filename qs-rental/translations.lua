ec06a92a0f5595c3e17189e3588bf04a68e38f6d
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:47:31 +0100

    Add files via upload

---
+++ qs-rental/translations.lua
@@ -0,0 +1,33 @@
+Config.Language = 'fr'
+
+Config.Languages = {
+    ['es'] = {
+        ["NOT_ENOUGH_MONEY"] = "You don't have enough money in the bank.",
+        ["RENTEL_SUCCESS"] = "Your rental car is waiting for you at",
+        ["RENTEL_ERROR"] = "You don't have money in your bank",
+        ["DELIVER_BACK"] = "You returned the vehicle to ",
+        ["RENTAL"] = "RENTAL",
+        ["RENT"] = "[E] - Rent a vehicle",
+        ["STORED"] = " - Stored"
+ 	},
+
+    ['en'] = {
+        ["NOT_ENOUGH_MONEY"] = "No tienes suficiente dinero en el banco.",
+        ["RENTEL_SUCCESS"] = "Tu vehiculo alquilado te está esperando en el",
+        ["RENTEL_ERROR"] = "No tienes dinero en tu banco",
+        ["DELIVER_BACK"] = "Devolvio el vehiculo a ",
+        ["RENTAL"] = "Rental",
+        ["RENT"] = "[E] - Rentar vehiculo",
+        ["STORED"] = " - Stored"
+ 	},
+
+  ['fr'] = {
+      ["NOT_ENOUGH_MONEY"] = "Vous n'avez pas assez d'argent.",
+      ["RENTEL_SUCCESS"] = "Le véhicule à été loué.",
+      ["RENTEL_ERROR"] = "Vous n'avez pas assez d'argent sur votre compte en banque.",
+      ["DELIVER_BACK"] = "Vous avez rendu le véhicule ",
+      ["RENTAL"] = "Location",
+      ["RENT"] = "Appuyez sur [E] pour louer un véhicule.",
+      ["STORED"] = " - Rangé"
+},
+}
