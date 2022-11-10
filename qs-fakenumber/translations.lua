37fa2f9388c8ed94ac54c6248dc3c4deb188b14d
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:30:51 +0100

    Add files via upload

---
+++ qs-fakenumber/translations.lua
@@ -0,0 +1,25 @@
+Config.Language = "fr" --You can choose between 'en', 'es' or create your own language.
+
+Config.Languages = { --You can copy one of these translations and create your own with another language.
+    ['en'] = {
+        ["MENU_TITLE"] = "SIM cards",
+        ["MENU_NUMBER"] = "Number:",
+        ["MENU_USE_NUMBER"] = "Use number",
+        ["NO_SIM"] = "You don't have SIM cards",
+        ["CHANGE_NUMBER_SUCCESS"] = "Your SIM card has been changed successfully:",
+        ["CHANGE_NUMBER_OLD"] = "your old SIM card:",
+        ["NEW_SIM"] = "New sim installed: ",
+        ["EXIT"] = "Exit",
+    },
+
+    ['fr'] = {
+        ["MENU_TITLE"] = "Carte SIM",
+        ["MENU_NUMBER"] = "Nouveau numéro:",
+        ["MENU_USE_NUMBER"] = "Utiliser un numéro",
+        ["NO_SIM"] = "Vous n'avez pas de cartes SIM.",
+        ["CHANGE_NUMBER_SUCCESS"] = "Numéro changé:",
+        ["CHANGE_NUMBER_OLD"] = "Ancien numéro:",
+        ["NEW_SIM"] = "Nouvelle carte SIM installée: ",
+        ["EXIT"] = "Quitter",
+    },
+}
