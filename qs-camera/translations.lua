1287f95a5194759a49a2fd627597aa6adfe38b65
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:23:30 +0100

    Add files via upload

---
+++ qs-camera/translations.lua
@@ -0,0 +1,39 @@
+--████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+--╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+--░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+--░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+--░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+--░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+Config.Language = 'fr'
+
+Config.Languages = {
+    ['en'] = {
+        ["CAMERA_INFORMATION_LOCATION"] = "Location:",
+        ["CAMERA_INFORMATION_DATE"] = "Date:",
+
+        ["CAMERA_NOTIFICATION_SUCCESS"] = "You took a photo at:",
+        ["CAMERA_NOTIFICATION_BROKEN"] = "Your camera was broken for some reason",
+        ["CAMERA_NOTIFICATION_REPAIR"] = "Your camera now works again",
+        ["CAMERA_NOTIFICATION_NO_CAMERA"] = "You don't have a broken camera",
+    },
+
+    ['es'] = {
+        ["CAMERA_INFORMATION_LOCATION"] = "Ubicación:",
+        ["CAMERA_INFORMATION_DATE"] = "Fecha:",
+
+        ["CAMERA_NOTIFICATION_SUCCESS"] = "Tomaste una foto en:",
+        ["CAMERA_NOTIFICATION_BROKEN"] = "Tu cámara se rompió por algún motivo",
+        ["CAMERA_NOTIFICATION_REPAIR"] = "Tu cámara ahora funciona de nuevo",
+        ["CAMERA_NOTIFICATION_NO_CAMERA"] = "No tienes una cámara rota",
+	},
+  ['fr'] = {
+  ["CAMERA_INFORMATION_LOCATION"] = "Emplacement :",
+        ["CAMERA_INFORMATION_DATE"] = "Date :",
+
+        ["CAMERA_NOTIFICATION_SUCCESS"] = "Vous avez pris une photo à :",
+        ["CAMERA_NOTIFICATION_BROKEN"] = "Votre caméra est cassée pour une raison quelconque.",
+        ["CAMERA_NOTIFICATION_REPAIR"] = "Votre caméra fonctionne à nouveau.",
+        ["CAMERA_NOTIFICATION_NO_CAMERA"] = "Vous n'avez pas de caméra cassée.",
+    }
+}
