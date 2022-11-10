e323a5beb32282c9479b1535bf8f682d00abf33d
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:23:46 +0100

    Update translations.lua

--- qs-camera/translations.lua
+++ qs-camera/translations.lua
@@ -17,16 +17,6 @@ Config.Languages = {
         ["CAMERA_NOTIFICATION_REPAIR"] = "Your camera now works again",
         ["CAMERA_NOTIFICATION_NO_CAMERA"] = "You don't have a broken camera",
     },
-
-    ['es'] = {
-        ["CAMERA_INFORMATION_LOCATION"] = "Ubicación:",
-        ["CAMERA_INFORMATION_DATE"] = "Fecha:",
-
-        ["CAMERA_NOTIFICATION_SUCCESS"] = "Tomaste una foto en:",
-        ["CAMERA_NOTIFICATION_BROKEN"] = "Tu cámara se rompió por algún motivo",
-        ["CAMERA_NOTIFICATION_REPAIR"] = "Tu cámara ahora funciona de nuevo",
-        ["CAMERA_NOTIFICATION_NO_CAMERA"] = "No tienes una cámara rota",
-	},
   ['fr'] = {
   ["CAMERA_INFORMATION_LOCATION"] = "Emplacement :",
         ["CAMERA_INFORMATION_DATE"] = "Date :",
