aa74f883fcf4707689990309925d53c6f9c61d42
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:48:17 +0100

    Update translations.lua

--- qs-rental/translations.lua
+++ qs-rental/translations.lua
@@ -1,7 +1,7 @@
 Config.Language = 'fr'
 
 Config.Languages = {
-    ['en'] = {
+    ['es'] = {
         ["NOT_ENOUGH_MONEY"] = "No tienes suficiente dinero en el banco.",
         ["RENTEL_SUCCESS"] = "Tu vehiculo alquilado te está esperando en el",
         ["RENTEL_ERROR"] = "No tienes dinero en tu banco",
