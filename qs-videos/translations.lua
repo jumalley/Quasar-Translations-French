780380739ca88ca7a91cf300fd791cdefb422fa8
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:54:44 +0100

    Update translations.lua

--- qs-videos/translations.lua
+++ qs-videos/translations.lua
@@ -9,15 +9,6 @@
 Config.Language = 'fr' -- Default 'en', 'es, 'de' and 'pt' but you have more translations in html.
 
 Config.Languages = {
-    ['es'] = {
-        ["VIDEO_UPLOADED"] = "¡Vídeo subido!",
-        ["VIDEO_TOO_LONG"] = "El video es demasiado largo",
-
-        ["VIDEO_HELP_START"] = "Iniciar vídeo: ~INPUT_ATTACK~",
-        ["VIDEO_HELP_STOP"] = "Salir del modo camara: ~INPUT_CELLPHONE_CANCEL~",
-        ["VIDEO_HELP_POSITION"] = "Fontal/Trasera: ~INPUT_PHONE~",
-    },
-
     ['en'] = {
         ["VIDEO_UPLOADED"] = "Video uploaded!",
         ["VIDEO_TOO_LONG"] = "Video is too long",
