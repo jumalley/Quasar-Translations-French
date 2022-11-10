c0f880cf7b44495c2d02b0436836e0a8b92d7d82
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:40:37 +0100

    Add files via upload

---
+++ qs-licenses/translations.lua
@@ -0,0 +1,15 @@
+Config.Language = "fr"
+
+Config.Languages = { --You can copy one of these translations and create your own with another language.
+    ['en'] = {
+        ["LICENSE_SUCCESS"] = "Thanks for waiting, here is your new",
+
+        ["NO_MONEY"] = "You don't have enough money",
+    },
+
+    ['fr'] = {
+        ["LICENSE_SUCCESS"] = "Merci d'avoir attendu voici votre document.<br>",
+
+        ["NO_MONEY"] = "Vous n'avez pas assez d'argent.",
+    },
+}
