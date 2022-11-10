bb6a6f47ec42bc5d1764c1bcf24e63e8183146d7
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 02:44:33 +0100

    Add files via upload

---
+++ qs-parachute/translations.lua
@@ -0,0 +1,15 @@
+Config.Language = "fr" --You can choose between 'en', 'es' or create your own language.
+
+Config.Languages = { --You can copy one of these translations and create your own with another language.
+    ['en'] = {
+        ["NO_PARACHUTE"] = "You don't have a parachute equipped!",
+        ["USE_PARACHUTE"] = "Equipping parachute...",
+        ["RESET_PARACHUTE"] = "Removing parachute...",
+    },
+
+    ['fr'] = {
+        ["NO_PARACHUTE"] = "Vous n'avez pas de parachute.",
+        ["USE_PARACHUTE"] = "Equipement du parachute...",
+        ["RESET_PARACHUTE"] = "Enlevement du parachute...",
+    },
+}
