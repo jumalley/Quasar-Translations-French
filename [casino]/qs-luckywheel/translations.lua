720ce69ac88f3cbf03a7e5ddf31b8a02b06c8419
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 03:00:05 +0100

    Add files via upload

---
+++ [casino]/qs-luckywheel/translations.lua
@@ -0,0 +1,39 @@
+Config.Language = 'en'
+
+Config.Languages = {
+    ['en'] = {
+        ["LUCKY_WHEEL"] = "Apppuyez sur [E] pour faire tourner<br> la roue de la fortune.",
+
+        ["PRIZE_1"] = "Vous avez gagné<br> 5000 jetons.",
+        ["PRIZE_2"] = "Vous avez gagné<br> 10000 jetons.",
+        ["PRIZE_3"] = "Vous avez gagné<br> 20000 jetons.",
+        ["PRIZE_4"] = "Vous avez gagné<br> 30000 jetons.",
+        ["PRIZE_5"] = "Vous avez gagné<br> 40000 jetons.",
+        ["PRIZE_6"] = "Vous avez gagné<br> 50000 jetons.",
+        ["PRIZE_7"] = "Vous avez gagné<br> 70000 jetons.",
+        ["PRIZE_8"] = "Vous avez gagné<br> 100000 jetons.",
+        ["PRIZE_9"] = "Vous avez gagné<br> 150000 jetons.",
+        ["PRIZE_CAR"] = "Vous avez gagné<br> Voiture Import!",
+
+        ["TRY_AGAIN"] = "Vous avez perdu.",
+        ["NO_TICKETS"] = "Vous n'avez plus de tickets.<br><br>Vous pouvez en acheter sur la tablette disponible au guichet.",
+    },
+
+    ['es'] = {
+        ["LUCKY_WHEEL"] = "[E] - Lucky Wheel",
+
+        ["PRIZE_1"] = "¡Felicitaciones, ganó 50 fichas y 1 ticket!",
+        ["PRIZE_2"] = "¡Felicitaciones, ganó 100 fichas!",
+        ["PRIZE_3"] = "¡Felicitaciones, ganó 200 fichas!",
+        ["PRIZE_4"] = "¡Felicitaciones, ganó 300 fichas!",
+        ["PRIZE_5"] = "¡Felicitaciones, ganó 400 fichas!",
+        ["PRIZE_6"] = "¡Felicitaciones, ganó 500 fichas!",
+        ["PRIZE_7"] = "¡Felicitaciones, ganó 700 fichas!",
+        ["PRIZE_8"] = "¡Felicitaciones, ganó 1,000 fichas!",
+        ["PRIZE_9"] = "¡Felicitaciones, ganó 1,500 fichas!",
+        ["PRIZE_CAR"] = "¡Felicitaciones, ganó el JACKPOT CAR!",
+
+        ["TRY_AGAIN"] = "¡Lo siento, perdiste, inténtalo de nuevo!",
+        ["NO_TICKETS"] = "No tienes suficientes boletos",
+	}
+}
