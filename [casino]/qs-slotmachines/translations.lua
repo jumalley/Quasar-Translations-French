0f25f69cb2be7eeb154172351c0d01780e81b216
Ju <50602425+jumalley@users.noreply.github.com>
2022-11-10 03:01:45 +0100

    Add files via upload

---
+++ [casino]/qs-slotmachines/translations.lua
@@ -0,0 +1,27 @@
+Config.Language = 'fr'
+
+Config.Languages = {
+    ['en'] = {
+		['HELP_PLAY'] = '~INPUT_CELLPHONE_CANCEL~ Stand up\n~INPUT_FRONTEND_RDOWN~ ~INPUT_JUMP~ Spin\n~INPUT_CONTEXT~ Change camera',
+		['YOU_WIN'] = 'You win ',
+		["BETTING"] = "Betting ",
+		['CHIPS_NAME'] = 'Chips',
+		['NOT_ENOUGH_CHIPS'] = 'You don\'t have enought chips!',
+
+
+		["PLAY_SLOTMACHINES"] = "[E] - Slot Machine ~y~",
+		["PLAY_CHIPS_NAME"] = "~w~ chips",
+    },
+
+    ['fr'] = {
+    ['HELP_PLAY'] = '~INPUT_CELLPHONE_CANCEL~ Stand up\n~INPUT_FRONTEND_RDOWN~ ~INPUT_JUMP~ Spin\n~INPUT_CONTEXT~ Change camera',
+    ['YOU_WIN'] = 'Vous avez gagné ',
+    ["BETTING"] = "Pari en cours ",
+    ['CHIPS_NAME'] = 'jetons.',
+    ['NOT_ENOUGH_CHIPS'] = 'Vous n\'avez pas assez de jetons!',
+
+
+    ["PLAY_SLOTMACHINES"] = "[E] - Slot Machine ~y~",
+    ["PLAY_CHIPS_NAME"] = "~w~ jetons.",
+    }
+}
